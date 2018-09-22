6c28ccbb548879dcc7f12226389fbfb26cbc5b3c
Richard Gordon <richgordon95@gmail.com>
2018-09-22 14:50:42 -0400

    test new gulpfile

--- xxgulpfile.js
+++ xxgulpfile.js
@@ -1,28 +1,37 @@
+// add functionality to compile sounds folder into dist build
+
+
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    del = require('del'),
-    imagemin = require('gulp-imagemin'),
-    uglify = require('gulp-uglify'),
-    usemin = require('gulp-usemin'),
+
+    sass = require('gulp-sass'), // sass converter
+    autoprefixer = require('gulp-autoprefixer'), //adds vendor prefixes for css
+    concat = require('gulp-concat'),
+    del = require('del'), //for clean task
+    imagemin = require('gulp-imagemin'), //minify images
+    uglify = require('gulp-uglify'), //uglify
+    usemin = require('gulp-usemin'), //minify code
     rev = require('gulp-rev'),
     cleanCss = require('gulp-clean-css'),
     flatmap = require('gulp-flatmap'),
-    htmlmin = require('gulp-htmlmin'),
-    browserSync = require('browser-sync');
+    htmlmin = require('gulp-htmlmin'), //minify html
+    browserSync = require('browser-sync'); //live reload
 
 
     gulp.task('sass', function () {
   return gulp.src('./css/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'))
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./css/*.scss', ['sass']);
 });
-
+ //runs browsersync
 gulp.task('browser-sync', function () {
    var files = [
       './*.html',
@@ -48,19 +57,19 @@ gulp.task('default', ['browser-sync'], function() {
 gulp.task('clean', function() {
     return del(['dist']);
 });
-
+ //copy fonts
 gulp.task('copyfonts', function() {
-   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof}*')
    .pipe(gulp.dest('./dist/fonts'));
 });
 
 // Images
 gulp.task('imagemin', function() {
-  return gulp.src('img/*.{png,jpg,gif}')
+  return gulp.src('img/*.{svg,png,jpg,gif}')
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
     .pipe(gulp.dest('dist/img'));
 });
-
+  //takes minified img for dist
 gulp.task('usemin', function() {
   return gulp.src('./*.html')
   .pipe(flatmap(function(stream, file){
@@ -75,7 +84,24 @@ gulp.task('usemin', function() {
     }))
     .pipe(gulp.dest('dist/'));
 });
-
+//builds dist folder and cleans up old one
 gulp.task('build',['clean'], function() {
     gulp.start('copyfonts','imagemin','usemin');
 });
+
+//added with udacity course
+gulp.task('scripts-dist', function () {
+  gulp.src('js/**/*.js')
+  .pipe(concat('all.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'));
+});
+
+//added with udacity course - makes producrtion version (minified, linted, etc.)
+gulp.task('dist', [
+//  'copy-html',
+  'imagemin',
+//  'styles',
+//  'lint',
+  'scripts-dist'
+]);
